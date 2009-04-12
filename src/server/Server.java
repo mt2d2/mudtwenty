88404aa4555e7f9e4f9c50e312ef3177b62e11cd
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-12 23:33:30 +0000

    Introduce the use of java.util.logging for server messages under the Logger "mudtwenty". Updated the reaper thread to avoid exceptions.

--- src/server/Server.java
+++ src/server/Server.java
@@ -5,35 +5,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Server
 {
+	/**
+	 * Port the server will run on this local host. In the future, this could be
+	 * read from a configuration file.
+	 */
 	private static final int	PORT	= 8080;
 
+	/**
+	 * Logging utility, globally addressed in entire program as "mudtwenty".
+	 * This could be agumented by using a global properties file to localize the
+	 * error strings.
+	 */
+	private static Logger		logger	= Logger.getLogger("mudtwenty");
+
+	private List<ServerThread>	clients;
 	private ServerSocket		serverSocket;
 	private boolean				done;
-	private List<ServerThread>	clients;
 
 	public Server() throws IOException
 	{
-		// Print the welcome message to the sever console
-		// TODO debugging?
-		this.printWelcomeMessage();
+		// Display welcome message
+		logger.info("Booting server on localhost:" + PORT);
 
+		this.clients = new ArrayList<ServerThread>();
 		this.serverSocket = new ServerSocket(PORT);
 		this.done = false;
-		this.clients = new ArrayList<ServerThread>();
 
 		// main loop accepts clients, spawns new threads to handle each
 		this.acceptClients();
 
 		// close the server socket when finished
 		this.serverSocket.close();
-	}
-
-	private void printWelcomeMessage()
-	{
-		System.out.println("Booting server on localhost:" + PORT);
 	}
 
 	private void sendMessageToAllClients(String message)
@@ -56,12 +62,13 @@ public class Server
 				this.clients.add(newClient);
 				new Thread(newClient).start();
 
-				// TODO debugging
-				System.out.println("Got connection: " + socket);
+				logger.fine("Got connection at Socket: " + socket);
+				logger.finer("Handing with thread, client id: " + this.clients.indexOf(newClient));
 			}
 			catch (IOException e)
 			{
-				e.printStackTrace();
+				logger.warning("Error establishing socket connection.");
+				logger.fine("" + e.getStackTrace());
 			}
 
 			// reaper thread
@@ -70,20 +77,28 @@ public class Server
 				public void run()
 				{
 					List<ServerThread> toRemove = new ArrayList<ServerThread>();
-					
+
 					while (true)
 					{
 						for (ServerThread st : Server.this.clients)
-						{
 							if (st.getState() == State.DONE)
-							{
 								toRemove.add(st);
-								Server.this.sendMessageToAllClients("Removed client: " + st);
-							}
-						}
+						
+						for (ServerThread st : toRemove)
+							Server.this.sendMessageToAllClients("Client leaving: " + st);
 						
 						Server.this.clients.removeAll(toRemove);
 						toRemove.clear();
+						
+						// TODO thread sleeping every second? Timer instead?
+						try
+						{
+							Thread.sleep(1000);
+						}
+						catch (InterruptedException e)
+						{
+							e.printStackTrace();
+						}
 					}
 				}
 
@@ -102,7 +117,8 @@ public class Server
 		}
 		catch (IOException e)
 		{
-			e.printStackTrace();
+			logger.severe("Error establishing server on port " + PORT);
+			logger.throwing("Server", "main", e);
 		}
 	}
 }
