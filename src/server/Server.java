5b3426a02990255fa980c0537f2f125cccf8fe9e
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-13 22:15:19 +0000

    Moved ReaperThread out of constructor into its own class. For clarity.

--- src/server/Server.java
+++ src/server/Server.java
@@ -11,20 +11,19 @@ import java.util.logging.Logger;
 public class Server
 {
 	public static final Color	SYSTEM_TEXT_COLOR	= new Color(0, 51, 0);
-	
+
 	/**
 	 * Logging utility, globally addressed in entire program as "mudtwenty".
 	 * This could be augmented by using a global properties file to localize the
 	 * error strings.
 	 */
-	private static final Logger	logger	= Logger.getLogger("mudtwenty");
+	private static final Logger	logger				= Logger.getLogger("mudtwenty");
 
 	/**
 	 * Port the server will run on this local host. In the future, this could be
 	 * read from a configuration file.
 	 */
-	private static final int	PORT	= 8080;
-	
+	private static final int	PORT				= 8080;
 
 	private List<ServerThread>	clients;
 	private ServerSocket		serverSocket;
@@ -40,51 +39,20 @@ public class Server
 		this.done = false;
 
 		// reaper thread
-		new Thread(new Runnable() {
-			@Override
-			public void run()
-			{
-				List<ServerThread> toRemove = new ArrayList<ServerThread>();
+		new Thread(new ReaperThread()).start();
 
-				while (true)
-				{
-					for (ServerThread st : Server.this.clients)
-						if (st.getState() == State.DONE)
-							toRemove.add(st);
-
-					Server.this.clients.removeAll(toRemove);
-
-					for (ServerThread st : toRemove)
-						Server.this.sendMessageToAllClients("client terminated connection: " + st, Server.SYSTEM_TEXT_COLOR);
-					
-					toRemove.clear();
-
-					// TODO thread sleeping every second? Timer instead?
-					try
-					{
-						Thread.sleep(1000);
-					}
-					catch (InterruptedException e)
-					{
-						e.printStackTrace();
-					}
-				}
-			}
-
-		}).start();
-		
 		// main loop accepts clients, spawns new threads to handle each
 		this.acceptClients();
 
 		// close the server socket when finished
 		this.serverSocket.close();
 	}
-	
+
 	public void sendMessageToAllClients(String message)
 	{
 		this.sendMessageToAllClients(message, null);
 	}
-	
+
 	public void sendMessageToAllClients(String message, Color color)
 	{
 		for (ServerThread st : this.clients)
@@ -112,6 +80,40 @@ public class Server
 				logger.warning("error establishing socket connection.");
 				logger.throwing("Server", "acceptClients", e);
 			}
+		}
+	}
+
+	private class ReaperThread implements Runnable
+	{
+		@Override
+		public void run()
+		{
+			List<ServerThread> toRemove = new ArrayList<ServerThread>();
+
+			while (true)
+			{
+				for (ServerThread st : Server.this.clients)
+					if (st.getState() == State.DONE)
+						toRemove.add(st);
+
+				Server.this.clients.removeAll(toRemove);
+
+				for (ServerThread st : toRemove)
+					Server.this.sendMessageToAllClients("client terminated connection: " + st, Server.SYSTEM_TEXT_COLOR);
+
+				toRemove.clear();
+
+				// TODO thread sleeping every second? Timer instead?
+				try
+				{
+					Thread.sleep(1000);
+				}
+				catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+			}
+
 		}
 	}
 
