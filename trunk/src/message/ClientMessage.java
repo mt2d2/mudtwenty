77a0b00404bd5964716fd8f0a4dda0e227fe178b
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-14 21:58:33 +0000

    Adjusted message protocol to use Externalizable for custom binary transmission. Added two _very rough_ test cases to test the speed thereof.

--- trunk/src/message/ClientMessage.java
+++ trunk/src/message/ClientMessage.java
@@ -1,6 +1,9 @@
 package message;
 
 import java.awt.Color;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
 public class ClientMessage extends Message
 {
@@ -8,30 +11,66 @@ public class ClientMessage extends Message
 	 * 
 	 */
 	private static final long	serialVersionUID	= 1L;
-	
-	private String payload;
-	private Color color;
+
+	private String				payload;
+	private Color				color;
+
+	public ClientMessage() // Externalizable requires blank constructor
+	{
+	}
 	
 	public ClientMessage(String payload)
 	{
 		this(payload, null);
 	}
-	
+
 	public ClientMessage(String payload, Color color)
 	{
 		super();
-		
+
 		this.payload = payload;
 		this.color = color;
 	}
-	
+
 	public String getPayload()
 	{
 		return this.payload;
 	}
-	
+
 	public Color getColor()
 	{
 		return this.color;
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException
+	{
+		// properties of message
+		// avoid a call to super for speed
+		this.time = in.readLong();
+		this.status = Status.valueOf(in.readUTF());
+
+		this.payload = in.readUTF();
+		
+		if (! in.readBoolean())
+			this.color = new Color(in.readInt());
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException
+	{
+		// properties of message
+		// avoid a call to super for speed
+		out.writeLong(this.time);
+		out.writeUTF(this.status.name());
+		
+		// write payload, non-nullable
+		out.writeUTF(this.payload);
+		
+		// write Color, nullable  
+		final boolean isColorNull = this.color == null;
+		out.writeBoolean(isColorNull);
+		if (! isColorNull)
+			out.writeInt(this.color.getRGB());	
 	}
 }
