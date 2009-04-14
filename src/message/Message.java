4c41b981860a71e2bb63d107030d634f4f09fa38
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-14 21:58:33 +0000

    Adjusted message protocol to use Externalizable for custom binary transmission. Added two _very rough_ test cases to test the speed thereof.

--- src/message/Message.java
+++ src/message/Message.java
@@ -1,12 +1,12 @@
 package message;
 
-import java.io.Serializable;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
-public abstract class Message implements Serializable 
+public abstract class Message implements Externalizable 
 {
-	/**
-	 * 
-	 */
 	private static final long	serialVersionUID	= 1L;
 	
 	protected long	time;
@@ -26,5 +26,19 @@ public abstract class Message implements Serializable
 	public Status getStatus()
 	{
 		return this.status;
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException
+	{
+		this.time = in.readLong();
+		this.status = Status.valueOf(in.readUTF());
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException
+	{
+		out.writeLong(this.time);
+		out.writeUTF(this.status.name());
 	}
 }
--- src/message/ServerMessage.java
+++ src/message/ServerMessage.java
@@ -1,18 +1,22 @@
 package message;
 
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ServerMessage extends Message
 {
-	/**
-	 * 
-	 */
 	private static final long	serialVersionUID	= 1L;
-	
 
-	private Command	command;
-	private List<String> arguments;
-	
+	private Command				command;
+	private List<String>		arguments;
+
+	public ServerMessage() // Externalizable requires blank constructor
+	{
+	}
+
 	public ServerMessage(Command command, List<String> arguments)
 	{
 		super();
@@ -25,9 +29,51 @@ public class ServerMessage extends Message
 	{
 		return this.command;
 	}
-	
+
 	public List<String> getArguments()
 	{
 		return this.arguments;
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException
+	{
+		// properties of message
+		// avoid a call to super for speed
+		// super.readExternal(in);
+		this.time = in.readLong();
+		this.status = Status.valueOf(in.readUTF());
+
+		// read command, non-nullable
+		this.command = Command.valueOf(in.readUTF());
+
+		// read arguments List, non-nullable
+		int arrayCount = in.readInt();
+		this.arguments = new ArrayList<String>();
+		// read arguments, nullable
+		if (arrayCount > 0)
+		{
+			for (int i = arrayCount; i > 0; i--)
+				this.arguments.add(in.readUTF());
+		}
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException
+	{
+		// properties of message
+		// avoid a call to super for speed
+		// super.writeExternal(out);
+		out.writeLong(this.time);
+		out.writeUTF(this.status.name());
+
+		// write command, non-nullable
+		out.writeUTF(this.command.name());
+
+		// write arguments List, non-nullable
+		out.writeInt(this.arguments.size());
+		// write arguments, nullable
+		for (String s : this.arguments)
+			out.writeUTF(s);
 	}
 }
