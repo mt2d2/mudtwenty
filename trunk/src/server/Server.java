fb51fcf8743b04a0d91829aad7a7c633dfbcfdb9
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-14 01:18:38 +0000

    Replaced while(true) Thread with Timer.

--- trunk/src/server/Server.java
+++ trunk/src/server/Server.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.logging.Logger;
 
 import message.ClientMessage;
@@ -16,30 +18,29 @@ import server.response.EchoResponse;
 import server.response.ServerResponse;
 import server.response.UnknownResponse;
 
-public class Server
-{
-	public static final Color	SYSTEM_TEXT_COLOR	= new Color(0, 51, 0);
+public class Server {
+	public static final Color SYSTEM_TEXT_COLOR = new Color(0, 51, 0);
 
 	/**
 	 * Logging utility, globally addressed in entire program as "mudtwenty".
 	 * This could be augmented by using a global properties file to localize the
 	 * error strings.
 	 */
-	private static final Logger	logger				= Logger.getLogger("mudtwenty");
+	private static final Logger logger = Logger.getLogger("mudtwenty");
 
 	/**
 	 * Port the server will run on this local host. In the future, this could be
 	 * read from a configuration file.
 	 */
-	private static final int	PORT				= 8080;
+	private static final int PORT = 8080;
 
-	private List<ServerThread>	clients;
-	private ServerSocket		serverSocket;
-	private boolean				done;
-	private Map<Command, ServerResponse>	actions;
-	
-	public Server() throws IOException
-	{
+	private List<ServerThread> clients;
+	private ServerSocket serverSocket;
+	private boolean done;
+	private Map<Command, ServerResponse> actions;
+	private Timer timer;
+
+	public Server() throws IOException {
 		// Display welcome message
 		logger.info("booting server on localhost:" + PORT);
 
@@ -47,9 +48,10 @@ public class Server
 		this.serverSocket = new ServerSocket(PORT);
 		this.installServerResponses();
 		this.done = false;
+		this.timer = new Timer();
 
 		// reaper thread
-		new Thread(new ReaperThread()).start();
+		timer.schedule(new ReaperTask(), 0, 1000);
 
 		// main loop accepts clients, spawns new threads to handle each
 		this.acceptClients();
@@ -58,98 +60,71 @@ public class Server
 		this.serverSocket.close();
 	}
 
-	private void installServerResponses()
-	{
+	private void installServerResponses() {
 		this.actions = new HashMap<Command, ServerResponse>();
 		this.actions.put(Command.ECHO, new EchoResponse());
 		this.actions.put(Command.UNKNOWN, new UnknownResponse());
 	}
 
-	public void sendMessageToAllClients(String message)
-	{
+	public void sendMessageToAllClients(String message) {
 		this.sendMessageToAllClients(message, null);
 	}
 
-	public void sendMessageToAllClients(String message, Color color)
-	{
+	public void sendMessageToAllClients(String message, Color color) {
 		for (ServerThread st : this.clients)
 			st.sendMessage(new ClientMessage(message, color));
 	}
-	
-	public ServerResponse getServerResponse(Command input)
-	{
+
+	public ServerResponse getServerResponse(Command input) {
 		return this.actions.get(input);
 	}
 
-	private void acceptClients()
-	{
+	private void acceptClients() {
 		Socket socket;
 		ServerThread newClient;
 
-		while (!this.done)
-		{
-			try
-			{
+		while (!this.done) {
+			try {
 				socket = serverSocket.accept();
 				newClient = new ServerThread(this, socket);
 				this.clients.add(newClient);
 				new Thread(newClient).start();
 
 				logger.info("client connected to server: " + socket);
-			}
-			catch (IOException e)
-			{
+			} catch (IOException e) {
 				logger.warning("error establishing socket connection.");
 				logger.throwing("Server", "acceptClients", e);
 			}
 		}
 	}
 
-	private class ReaperThread implements Runnable
-	{
+	private class ReaperTask extends TimerTask {
 		@Override
-		public void run()
-		{
+		public void run() {
 			List<ServerThread> toRemove = new ArrayList<ServerThread>();
 
-			while (true)
-			{
-				for (ServerThread st : Server.this.clients)
-					if (st.getState() == State.DONE)
-						toRemove.add(st);
+			for (ServerThread st : Server.this.clients)
+				if (st.getState() == State.DONE)
+					toRemove.add(st);
 
-				Server.this.clients.removeAll(toRemove);
+			Server.this.clients.removeAll(toRemove);
 
-				for (ServerThread st : toRemove)
-					Server.this.sendMessageToAllClients("client terminated connection: " + st, Server.SYSTEM_TEXT_COLOR);
+			for (ServerThread st : toRemove)
+				Server.this.sendMessageToAllClients(
+						"client terminated connection: " + st,
+						Server.SYSTEM_TEXT_COLOR);
 
-				toRemove.clear();
-
-				// TODO thread sleeping every second? Timer instead?
-				try
-				{
-					Thread.sleep(1000);
-				}
-				catch (InterruptedException e)
-				{
-					e.printStackTrace();
-				}
-			}
-
+			toRemove.clear();
 		}
 	}
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)
-	{
-		try
-		{
+	public static void main(String[] args) {
+		try {
 			new Server();
-		}
-		catch (IOException e)
-		{
+		} catch (IOException e) {
 			logger.severe("error establishing server on port " + PORT);
 			logger.throwing("Server", "main", e);
 		}
