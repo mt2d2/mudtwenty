f949ad7d9b849f0375d209270e875dae1db35648
qyearsley <qyearsley@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-14 02:38:22 +0000

    SVN test. No changes.

--- trunk/src/server/response/UnknownResponse.java
+++ trunk/src/server/response/UnknownResponse.java
@@ -9,7 +9,6 @@ import message.ClientMessage;
 public class UnknownResponse implements ServerResponse
 {
 
-	@Override
 	public ClientMessage respond(ServerThread serverThread, List<String> arguments)
 	{
 		return new ClientMessage("The command you entered was not understood. Type help for a list of available commands.");
