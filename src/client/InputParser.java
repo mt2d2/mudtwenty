c931eabf961a02700336f095bd81ff2e333608b5
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-15 06:27:17 +0000

    Added Javadoc for package client.

--- src/client/InputParser.java
+++ src/client/InputParser.java
@@ -5,13 +5,28 @@ import java.util.Arrays;
 import message.Command;
 import message.ServerMessage;
 
+/**
+ * Simple utility that parses a user's input into a ClientMessage that can be
+ * sent to the Server.
+ * 
+ * @author Michael Tremel (mtremel@email.arizona.edu)
+ */
 public class InputParser
 {
+	/**
+	 * Parses the input given by a user. Currently, the first word is tested as
+	 * a viable Command, the reset of the words are passed as arguments in the
+	 * message.
+	 * 
+	 * @param input
+	 *            user-provided input
+	 * @return message which can be sent to the server.
+	 */
 	public static final ServerMessage parse(String input)
 	{
 		String[] chunks = input.split(" ");
 		Command command = null;
-		
+
 		try
 		{
 			command = Command.valueOf(chunks[0].toUpperCase());
@@ -20,10 +35,19 @@ public class InputParser
 		{
 			command = Command.UNKNOWN;
 		}
-			
+
 		return new ServerMessage(command, Arrays.asList(removeElement(chunks, 0)));
 	}
 
+	/**
+	 * Removes an element from a an array yielding a new array with that data.
+	 * 
+	 * @param elementData
+	 *            original array
+	 * @param index
+	 *            item to remove
+	 * @return new array sized to fit
+	 */
 	private static String[] removeElement(String[] elementData, int index)
 	{
 		int size = elementData.length;
@@ -32,7 +56,7 @@ public class InputParser
 		int numMoved = size - index - 1;
 		if (numMoved > 0)
 			System.arraycopy(elementData, index + 1, newData, index, numMoved);
-	
+
 		elementData = null; // let the gc work
 		return newData;
 	}
