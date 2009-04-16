4f85961ab2f567865a3a1e53d865b52c20d7980e
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-16 05:58:54 +0000

    Updated javadoc and uml.

--- src/server/universe/Item.java
+++ src/server/universe/Item.java
@@ -2,9 +2,13 @@ package server.universe;
 
 /**
  * The Item interface represents anything in the world that's not a Creature --
- * anything that has no intelligence or ability to do thing like talking and fighting.
+ * anything that has no intelligence or ability to do thing like talking and
+ * fighting.
  */
 public interface Item
 {
-
+	/**
+	 * @return price of an item, in the standard money system
+	 */
+	public int getPrice();
 }
