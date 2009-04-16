5f12a24e13ca0c78c5e5ce544dddaf214fdc6261
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-16 06:04:08 +0000

    Updated UML. Javadoc for Session. Updated a few other classes along the way.

--- src/server/universe/Session.java
+++ src/server/universe/Session.java
@@ -5,32 +5,55 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Centralized session storage. This allows easy access to a players location
+ * with regard to Rooms.
+ * 
+ * @author Michael Tremel (mtremel@email.arizona.edu)
+ */
 public class Session
 {
-	private Map<Player, Room> playersToRooms;
-	private Map<Room, Player> roomsToPlayers;
-	
+	private Map<Player, Room>	playersToRooms;
+	private Map<Room, Player>	roomsToPlayers;
+
+	/**
+	 * Sole constructor, sets Session up for use.
+	 */
 	public Session()
 	{
 		this.playersToRooms = new HashMap<Player, Room>();
 		this.roomsToPlayers = new HashMap<Room, Player>();
 	}
-	
+
+	/**
+	 * @param player
+	 *            a Player to locate
+	 * @return the current location of a Player
+	 */
 	public Room getRoomOfPlayer(Player player)
 	{
 		return this.playersToRooms.get(player);
 	}
-	
+
+	/**
+	 * Searches for players in a room. This could easily be extend to support
+	 * searching for all entities instead.
+	 * 
+	 * @param room
+	 *            this will be searched for all players in a the room and
+	 *            returns them.
+	 * @return list of players present in the room
+	 */
 	public List<Player> getPlayersInRoom(Room room)
 	{
 		List<Player> toReturn = new ArrayList<Player>();
-		
+
 		for (Map.Entry<Room, Player> entry : this.roomsToPlayers.entrySet())
 		{
 			if (entry.getKey().equals(room))
 				toReturn.add(entry.getValue());
 		}
-		
+
 		return toReturn;
 	}
 }
--- src/server/universe/Skill.java
+++ src/server/universe/Skill.java
@@ -1,20 +1,21 @@
 package server.universe;
 
 /**
- * The Skill class represents a skill. A skill can be checked, and some Interaction can
- * fail or succeed based on this. Usually, but not necessarily, when a skill is checked
- * it is practiced.
+ * The Skill class represents a skill. A skill can be checked, and some
+ * Interaction can fail or succeed based on this. Usually, but not necessarily,
+ * when a skill is checked it is practiced.
  */
 public class Skill
 {
 
-	private String name;
-	private int value;
+	private String	name;
+	private int		value;
 
 	/**
 	 * Create a new skill with the given attributes.
 	 */
-	public Skill(String name, int initValue) {
+	public Skill(String name, int initValue)
+	{
 		this.name = name;
 		this.value = initValue;
 	}
@@ -22,28 +23,29 @@ public class Skill
 	/**
 	 * @return The name of the skill.
 	 */
-	public String getName() {
+	public String getName()
+	{
 		return name;
 	}
 
 	/**
-	 * @return The current value of the skill.
-	 * //TODO what units?
+	 * @return The current value of the skill. //TODO what units?
 	 */
-	public int getValue() {
+	public int getValue()
+	{
 		return value;
 	}
 
 	/**
-	 * Practice the skill. This has a chance of increasing the value of the skill.
+	 * Practice the skill. This has a chance of increasing the value of the
+	 * skill.
 	 */
-	public void practice() {
+	public void practice()
+	{
 		/*
-		TODO
-		The result of practicing a skill should depend on the current skill level
-		and it should have a chance of increasing the skill.
-		*/
+		 * TODO The result of practicing a skill should depend on the current
+		 * skill level and it should have a chance of increasing the skill.
+		 */
 	}
-
 
 }
