89ecd3da5bae35b5f9acccf343c1cd8137548dd9
qyearsley <qyearsley@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-23 05:33:53 +0000

    added skeleton of methods in Universe

--- trunk/src/server/universe/Universe.java
+++ trunk/src/server/universe/Universe.java
@@ -1,31 +1,43 @@
 package server.universe;
 
-import java.util.ArrayList;
+import java.io.Serializable;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * A Universe represents the entire state of the virtual world. A universe
  * contains Rooms and Entities. Universe is a singleton class -- there is no
  * public constructor, but
- * 
+ *
  * The Universe keeps track
  */
-public class Universe
+public class Universe implements Serializable
 {
-	private Map<Creature, Room>	creatureToRoom;
-	private Map<Room, Creature>	roomToCreature;
-	
+	/**
+	 * A map to get a list of <b>all players, even logged-out players,</b> in a given room.
+	 */
+	private Map<Room, List<Player>> roomToPlayers;
+
+	/**
+	 * A map to get a list of mobs in a given room.
+	 */
+	private Map<Room, List<MOB>> roomToMOBs;
+
 	private static Universe		theUniverse;
 	private List<Player>		players;
 	private List<Room>			rooms;
 
 	/**
-	 * In this private constructor, the universe should be loaded from a file if
-	 * there is one. Otherwise, a default simple universe should be made.
+	 * In this private constructor, a simple default universe might be made.
+	 * The universe will not be loaded here, because it should be loaded from
+	 * a file by Server.
 	 */
 	private Universe()
 	{
+		this.roomToPlayers = new HashMap<Room, List<Player>>();
+		this.roomToMOBs = new HashMap<Room, List<MOB>>();
 		this.rooms = new ArrayList<Room>();
 		this.players = new ArrayList<Player>();
 	}
@@ -46,8 +58,32 @@ public class Universe
 	}
 
 	/**
+	 * Return a list of all <b>currently logged in</b> players in a room.
+	 */
+	public List<Player> getPlayersInRoom(Room room)
+	{
+		return null;
+	}
+
+	/**
+	 * Return a list of all MOBs in a room.
+	 */
+	public List<Player> getMOBsInRoom(Room room)
+	{
+		return null;
+	}
+
+	/**
+	 * Return a list of all <i>currently logged in</i> players in a room.
+	 */
+	public List<Player> getRoomOfCreature(Creature creature)
+	{
+		return null;
+	}
+
+	/**
 	 * Return a list of all rooms in the universe.
-	 * 
+	 *
 	 * @return A list of all of the rooms in the universe.
 	 */
 	public List<Room> getRooms()
@@ -57,7 +93,7 @@ public class Universe
 
 	/**
 	 * Return a list of players.
-	 * 
+	 *
 	 * @return A list of all of the players currently logged in to the universe.
 	 */
 	public List<Player> getPlayers()
@@ -69,7 +105,7 @@ public class Universe
 	 * Adds a player to this Universe. Currently only adds the player to the
 	 * list of players, but this could be useful to start the player off in a
 	 * default room or something.
-	 * 
+	 *
 	 * @param player
 	 *            to add
 	 */
@@ -80,7 +116,7 @@ public class Universe
 
 	/**
 	 * Removes a player from the list of currently logged-in players.
-	 * 
+	 *
 	 * @param player
 	 *            to remove
 	 */
