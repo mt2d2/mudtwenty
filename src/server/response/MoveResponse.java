b92ebb20526c4d763cace2ac20a544db30d5f2c9
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-24 07:33:53 +0000

    inital move support

---
+++ src/server/response/MoveResponse.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package server.response;
+
+import java.util.List;
+
+import message.ClientMessage;
+import server.Server;
+import server.ServerThread;
+
+/**
+ * Responds to the move command as input by the user. Specifically, this will
+ * move a user from his current room, to the room which is connected by an exit
+ * the user selects.
+ * 
+ * @author Michael Tremel (mtremel@email.arizona.edu)
+ */
+public class MoveResponse implements ServerResponse
+{
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see server.response.ServerResponse#respond(server.ServerThread,
+	 * java.util.List)
+	 */
+	@Override
+	public ClientMessage respond(ServerThread serverThread, List<String> arguments)
+	{
+		if (arguments.size() != 1)
+		{
+			return new ClientMessage("proper syntax of move: move <exit name>", Server.ERROR_TEXT_COLOR);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+}
