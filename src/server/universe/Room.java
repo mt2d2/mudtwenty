1fe8fc2150a784fe4a54c0090ab8743f501da47d
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-24 04:21:15 +0000

    gave description to room

--- src/server/universe/Room.java
+++ src/server/universe/Room.java
@@ -4,24 +4,40 @@ import java.util.List;
 
 /**
  * A Room represents a location in the universe.
- *
+ * 
  * Thus quoteth The Spec:
- *
- * "Rooms in a MUD should be thought of as locations rather than an indoor area with walls and a ceiling
- * (A "room" could be a clearing in the woods with a path leading to the north and another to the west,
- * or the middle of a field with paths leading in all directions). Rooms have exits that connect them to
+ * 
+ * "Rooms in a MUD should be thought of as locations rather than an indoor area
+ * with walls and a ceiling (A "room" could be a clearing in the woods with a
+ * path leading to the north and another to the west, or the middle of a field
+ * with paths leading in all directions). Rooms have exits that connect them to
  * other rooms, descriptions, and contents (including items, players and MOBs)."
  */
-public class Room
+public class Room implements Entity
 {
-	private List<Exit> exits;
-	private List<Item> items;
+	private List<Exit>	exits;
+	private List<Item>	items;
+	private String		name;
+	private String		blurb;
 
 	/**
 	 * Create a new room with the given attributes.
+	 * 
+	 * @param name
+	 *            name of the room users will see and reference to
+	 * @param blurb
+	 *            small blurb of what the room looks like, i.e., decoration,
+	 *            weather, etc.
+	 * @param exits
+	 *            list of exists the room has
+	 * @param items
+	 *            list of items the room has
 	 */
-	public Room(List<Exit> exits, List<Item> items)
+	public Room(String name, String blurb, List<Exit> exits, List<Item> items)
 	{
+		this.name = name;
+		this.blurb = blurb;
+
 		this.exits = exits;
 		this.items = items;
 	}
@@ -43,9 +59,8 @@ public class Room
 	}
 
 	/**
-	 * Add an item to the room.
-	 * Rooms can contain infinitely many items!!
-	 * Or rather, Room storage capacity is bounded by computer memory.
+	 * Add an item to the room. Rooms can contain infinitely many items!! Or
+	 * rather, Room storage capacity is bounded by computer memory.
 	 */
 	public void addItem(Item item)
 	{
@@ -60,4 +75,29 @@ public class Room
 		this.items.remove(item);
 	}
 
+	@Override
+	public String getDescription()
+	{
+		StringBuilder toReturn = new StringBuilder();
+
+		toReturn.append("Room: " + this.name + "\n");
+
+		toReturn.append("Description" + this.blurb + "\n");
+
+		toReturn.append("Exits:\n");
+		for (Exit e : this.exits)
+			toReturn.append("\t" + e.getDescription() + "\n");
+
+		toReturn.append("Items:\n");
+		for (Item i : this.items)
+			toReturn.append("\t" + i.getDescription());
+
+		return toReturn.toString();
+	}
+
+	@Override
+	public String getName()
+	{
+		return this.name;
+	}
 }
