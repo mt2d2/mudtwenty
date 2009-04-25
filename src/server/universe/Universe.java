6ee42efdab88b3fa1de0cb389ae9e2c21017a763
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-25 22:12:09 +0000

    removed all hashing procedures from login and registration, fixed bug in InputParser with an empty list

--- src/server/universe/Universe.java
+++ src/server/universe/Universe.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * A Universe represents the entire state of the virtual world. A universe
  * contains Rooms and Entities. Universe is a singleton class -- there is no
  * public constructor; instances are gotten with getInstance().
- *
+ * 
  * The Universe keeps track of all the creatures and their locations, including
  * the players, and whether the players are logged in or not.
  */
@@ -113,7 +113,7 @@ public class Universe implements Serializable
 
 	/**
 	 * Return a list of players that are currently logged in.
-	 *
+	 * 
 	 * @return A list of all of the players currently logged in to the universe.
 	 */
 	public List<Player> getLoggedInPlayers()
@@ -134,6 +134,9 @@ public class Universe implements Serializable
 	/**
 	 * Register a player with this name. Put the player in the starting room.
 	 * But, do not log them in yet.
+	 * 
+	 * @param name
+	 *            username of the player
 	 */
 	public void register(String name)
 	{
@@ -143,6 +146,9 @@ public class Universe implements Serializable
 	/**
 	 * Remove a player from the list of registered players. If the player with
 	 * the given name is in the game right now, boot them.
+	 * 
+	 * @param name
+	 *            username of the player
 	 */
 	public void unregister(String name)
 	{
@@ -153,10 +159,10 @@ public class Universe implements Serializable
 	 * Adds a player to the list of logged-in players in the Universe. Tell the
 	 * server that a user is now logged in. The Universe can now find a rightful
 	 * place for the user.
-	 *
+	 * 
 	 * If the Player was not previously in the list of players on the server, it
 	 * will now be added and put into the starting room.
-	 *
+	 * 
 	 * @param player
 	 *            to add
 	 */
@@ -168,11 +174,11 @@ public class Universe implements Serializable
 
 	/**
 	 * Removes a player from the list of currently logged-in players.
-	 *
+	 * 
 	 * This removes the player from the world, but it doesn't lose track of the
 	 * player's location. It does not delete a Player from the list of Players
 	 * in the Universe.
-	 *
+	 * 
 	 * @param player
 	 *            to remove
 	 */
@@ -180,11 +186,11 @@ public class Universe implements Serializable
 	{
 		this.playerToRoom.remove(player);
 	}
- 
+
 	/**
 	 * Called by Java during deserialization. This helps in restoring transient
 	 * fields.
-	 *
+	 * 
 	 * @param stream
 	 * @throws IOException
 	 * @throws ClassNotFoundException
