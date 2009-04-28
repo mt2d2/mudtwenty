6287a2b960378809e84275274c5b2b2e2555f7fd
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-28 06:31:57 +0000

    removed unused import to remove warnings

--- src/client/ClientThread.java
+++ src/client/ClientThread.java
@@ -6,7 +6,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ConnectException;
 import java.net.Socket;
-import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.text.DateFormat;
 import java.util.Date;
