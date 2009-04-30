052b71974794846e664d06c92f7743c4271d700c
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-30 04:12:39 +0000

    remove warnings associated with serialization

--- trunk/src/server/universe/Armor.java
+++ trunk/src/server/universe/Armor.java
@@ -2,24 +2,29 @@ package server.universe;
 
 /**
  * Armor can be equipped to creatures.
+ * 
  * @author Simon
- *
+ * 
  */
-public class Armor implements Item {
+public class Armor implements Item
+{
+	private static final long	serialVersionUID	= 1L;
 
 	@Override
-	public int getPrice() {
+	public int getPrice()
+	{
 		return 5;
 	}
 
 	@Override
-	public String getDescription() {
+	public String getDescription()
+	{
 		return "Protect yourself from enemy attacks";
 	}
 
 	@Override
-	public String getName() {
+	public String getName()
+	{
 		return "leather";
 	}
-
 }
