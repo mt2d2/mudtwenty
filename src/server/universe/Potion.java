9d68ad5a6ef0b92ac67432a6c721f3be1eb8ee81
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-04-30 04:12:39 +0000

    remove warnings associated with serialization

--- src/server/universe/Potion.java
+++ src/server/universe/Potion.java
@@ -3,24 +3,30 @@ package server.universe;
 /**
  * Potion class outlines everything a potion does.
  */
-public class Potion implements Item {
+public class Potion implements Item
+{
+	private static final long	serialVersionUID	= 1L;
 
 	@Override
-	public int getPrice() {
+	public int getPrice()
+	{
 		return 5;
 	}
 
 	@Override
-	public String getDescription() {
+	public String getDescription()
+	{
 		return "Use potions to recover health points";
 	}
 
 	@Override
-	public String getName() {
+	public String getName()
+	{
 		return "small potion";
 	}
 
-	public int getHealingPower() {
+	public int getHealingPower()
+	{
 		return 10;
 	}
 }
--- src/server/universe/Weapon.java
+++ src/server/universe/Weapon.java
@@ -2,24 +2,29 @@ package server.universe;
 
 /**
  * Outline of item weapon that can be equipped to creatures.
+ * 
  * @author Simon
- *
+ * 
  */
-public class Weapon implements Item {
+public class Weapon implements Item
+{
+	private static final long	serialVersionUID	= 1L;
 
 	@Override
-	public int getPrice() {
+	public int getPrice()
+	{
 		return 5;
 	}
 
 	@Override
-	public String getDescription() {
+	public String getDescription()
+	{
 		return "Attack enemies with extra power";
 	}
 
 	@Override
-	public String getName() {
+	public String getName()
+	{
 		return "dagger";
 	}
-
 }
