f418fac150dc9c06960ea0d9c9ead42ec0e92e7e
qyearsley <qyearsley@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-01 01:34:59 +0000

    More initial work on MOBs. (a couple method skeletons)

---
+++ trunk/src/server/universe/DialogStrategy.java
@@ -0,0 +1,20 @@
+package server.universe;
+
+import java.io.Serializable;
+
+/**
+ * A BehaviorStrategy represents a behavior that a MOB can have at any given time.
+ * For example, a different behaviors could be the behaviors
+ * of moving/fighting, running away, or staying still.
+ */
+public interface DialogStrategy extends Serializable
+{
+
+	/**
+	 * This method will be called whenever somebody says something to a MOB.
+	 * It will then probably 'say' or 'tell' something in response to this
+	 * message.
+	 */
+	public void reply(Creature sender, String message);
+
+}
