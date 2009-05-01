d9c1b76c05dff8a4d4fdd16796e5f1cf6df3088f
qyearsley <qyearsley@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-01 23:10:22 +0000

    Modified look to list mob names

--- trunk/src/server/response/LookResponse.java
+++ trunk/src/server/response/LookResponse.java
@@ -1,10 +1,12 @@
 package server.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import message.ClientMessage;
 import server.ServerThread;
 import server.Server;
+import server.universe.Entity;
 import server.universe.Player;
 import server.universe.Room;
 
@@ -22,24 +24,62 @@ public class LookResponse implements ServerResponse
 	 */
 	public ClientMessage respond(ServerThread serverThread, List<String> arguments)
 	{
+		
 		StringBuilder message = new StringBuilder();
-		final Room roomOfPlayer = Server.getUniverse().getRoomOfCreature(serverThread.getPlayer());
+		Player player = serverThread.getPlayer();
+		final Room roomOfPlayer = Server.getUniverse().getRoomOfCreature(player);
 		
 		// Append room info.
 		message.append(roomOfPlayer.getDescription());
 
 		// Append players in room.
-		message.append("\tPlayers in this room: ");
-		final List<Player> playersInRoom = Server.getUniverse().getPlayersInRoom(roomOfPlayer);
-		for (Player player : playersInRoom)
-			message.append(player.getName() + ", ");
-
-		// remove trailing comma or report that there are no players
-		if (playersInRoom.size() >= 1)
-			message.replace(message.length() - 2, message.length(), " ");
-		else
-			message.append("no users in the room");
+		message.append(players(player, roomOfPlayer) + "\n");
+		message.append(mobs(roomOfPlayer));
 
 		return new ClientMessage(message.toString());
+	}
+	
+	/**
+	 * Get a string description of players in the room.
+	 */
+	private String players(Player player, Room room)
+	{
+		
+		List<Entity> players = new ArrayList<Entity>();
+		players.addAll(Server.getUniverse().getPlayersInRoom(room));
+		
+		players.remove(player); // don't include the player that's looking.
+		if (players.isEmpty())
+			return "There are no other players in this room.";
+		else
+			return "Players in the room: " + listEntityNames(players);
+	}
+	
+	/**
+	 * Get a string description of mobs in the room.
+	 */
+	private String mobs(Room room)
+	{
+		List<Entity> mobs = new ArrayList<Entity>();
+		mobs.addAll(Server.getUniverse().getMOBsInRoom(room));
+		
+		if (mobs.isEmpty())
+			return "There are no mobs in this room.";
+		else
+			return "MOBs in the room: " + listEntityNames(mobs);
+	}
+	
+	/**
+	 * Given a list of entities, return a properly formatted string representation. 
+	 */
+	private String listEntityNames(List<Entity> entities)
+	{
+		if (entities.isEmpty())
+			return "None.";
+		else if (entities.size() == 1)
+			return entities.get(0).getName();
+		else
+			return entities.get(0).getName() + ", "
+				+ listEntityNames(entities.subList(1, entities.size()-1));
 	}
 }
