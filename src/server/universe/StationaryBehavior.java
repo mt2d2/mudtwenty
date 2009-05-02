77af97c483ac4724818822e3d8f50f71d1acdf3b
qyearsley <qyearsley@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-02 05:08:51 +0000

    Remembered to add StationaryBehavior for Kitten. Will not forget such again.

---
+++ src/server/universe/StationaryBehavior.java
@@ -0,0 +1,17 @@
+package server.universe;
+
+/**
+ * The simplest behavior that a MOB can have: stay still and don't do anything. 
+ */
+public class StationaryBehavior implements BehaviorStrategy {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Do the action of doing nothing. How zen.
+	 */
+	public void doAction(MOB mob)
+	{
+	}
+
+}
