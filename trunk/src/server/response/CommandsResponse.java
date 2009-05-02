57cc58d6ef2d8dbe44dcd85a42cfc4a69a973f7c
qyearsley <qyearsley@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-02 02:23:36 +0000

    Replaced login/register commands with auto login/registration in ServerThread

--- trunk/src/server/response/CommandsResponse.java
+++ trunk/src/server/response/CommandsResponse.java
@@ -18,24 +18,30 @@ public class CommandsResponse implements ServerResponse {
 	public ClientMessage respond(ServerThread serverThread, List<String> arguments)
 	{
 		String message = "The following commands are available:\n"
-				+ "\tquit, exit: saves your state and exits the mud\n"
-				+ "\tscore, sc: gives detailed information about your player\n"
-				+ "\tooc <message>: send a message to all users connected to the mud\n"
-				+ "\tcommands, help :lists all available commands and general system help\n"
+				+ "Chat commands:\n"
 				+ "\twho, w: lists all users online, both users and guests\n"
-				+ "\tlogin <username> <password>: logs a user in\n"
-				+ "\tregister <username> <password>: registers a user\n"
 				+ "\ttell <name> <message>: send a private message to a player or mob\n"
 				+ "\tsay <message>: send a message to everyone in the same room\n"
+				+ "\tooc <message>: send a message to all users connected to the mud\n"
+				+ "General commands:\n"
+				+ "\tcommands, help :lists all available commands and general system help\n"
+				+ "\tquit, exit: saves your state and exits the mud\n"
+				+ "\tscore, sc: gives detailed information about your player\n"
 				+ "\tlook, ls, l: look around the room, giving a description\n"
 				+ "\tlook, ls, l <entity>: examine an entity, giving its description\n"
 				+ "\tinventory, inv: list all the items you are currently carrying\n"
-				+ "\tdrop <item>: drops a specified item from your inventory, e.g., drop <item name>\n"
-				+ "\tuse <item>: use a specified item from your inventory, e.g., use <item name>\n"
-				+ "\tmove <direction>: moves you to through your specified exit\n"
-				+ "\tshutdown: admin command: shuts down the server, saving and notifying players\n"
+				+ "\tdrop <item>: drop a specified item from your inventory\n"
+				+ "\tuse <item>: use a specified item from your inventory\n"
 				+ "\tgive <name> <item>: gives an item to a user\n"
-				+ "\tget <item>: gets an item that exists in the room\n";
+				+ "\tget <item>: gets an item that exists in the room\n"
+				+ "Movement commands:\n"
+				+ "\tmove <direction>: move you to through your specified exit\n"
+				+ "\tnorth: move north\n"
+				+ "\tsouth: move south\n"
+				+ "\teast: move east\n"
+				+ "\twest: move west\n"
+				+ "Admin commands:\n"
+				+ "\tshutdown: shuts down the server, saving and notifying players\n";
 		
 		return new ClientMessage(message);
 	}
