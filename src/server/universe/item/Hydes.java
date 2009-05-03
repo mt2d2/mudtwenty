84599ed89fe3d332be336fe81225613b8a7dab13
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-03 23:02:31 +0000

    did anyone try running this before?

--- src/server/universe/item/Hydes.java
+++ src/server/universe/item/Hydes.java
@@ -3,30 +3,34 @@ package server.universe.item;
 import message.ClientMessage;
 import server.universe.Player;
 
-public abstract class Hydes implements Item{
+public abstract class Hydes extends Item
+{
+	private static final long	serialVersionUID	= 1L;
 
 	@Override
-	public int getPrice() {
+	public int getPrice()
+	{
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
-	public ClientMessage use(Player player) {
+	public ClientMessage use(Player player)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String getDescription() {
+	public String getDescription()
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public String getName() {
+	public String getName()
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
