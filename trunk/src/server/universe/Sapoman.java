69567feb168b389755c4ea6efde91bfda5720725
paretoparadise <paretoparadise@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-03 22:07:31 +0000

    Species changed I'm also go in to add to armors. I'm assuming that are mud is space opera themed

---
+++ trunk/src/server/universe/Sapoman.java
@@ -0,0 +1,83 @@
+package server.universe;
+
+public class Sapoman implements Species{
+	
+	private long health = 30;
+	private long mana = 0;
+	private long intel = 15;
+	private long STR = 5;
+	private long DEF = 5;
+	private short HIT = 5;
+	private long ATK = 5;
+	private long mres = 0;
+	private double gFactor = 0.0001;
+	private String descript = new String("This is a humanoid species, they lack in" +
+			" magical skill, magic defense, and are average mostly. Though they are " + 
+			"somewhat intelligent");
+	
+	
+	public Sapoman(){
+		
+	}
+
+	@Override
+	public long getATK() {
+		// TODO Auto-generated method stub
+		return ATK;
+	}
+
+	@Override
+	public long getDEF() {
+		// TODO Auto-generated method stub
+		return DEF;
+	}
+
+	@Override
+	public double getGFactor() {
+		// TODO Auto-generated method stub
+		return gFactor;
+	}
+
+	@Override
+	public short getHIT() {
+		// TODO Auto-generated method stub
+		return HIT;
+	}
+
+	@Override
+	public long getHealth() {
+		// TODO Auto-generated method stub
+		return health;
+	}
+
+	@Override
+	public long getMRes() {
+		// TODO Auto-generated method stub
+		return mres;
+	}
+
+	@Override
+	public long getMana() {
+		// TODO Auto-generated method stub
+		return mana;
+	}
+
+	@Override
+	public long getSTR() {
+		// TODO Auto-generated method stub
+		return STR;
+	}
+
+	@Override
+	public void setFactor(double chg) {
+		// TODO Auto-generated method stub
+		gFactor = chg;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
