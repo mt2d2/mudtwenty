b938a65f33fceb87044dc826cddf2fa15b0743ed
qyearsley <qyearsley@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-05 22:09:10 +0000

    Fixed a couple comments, removed Species from repository.

--- src/message/ServerMessage.java
+++ src/message/ServerMessage.java
@@ -18,18 +18,15 @@ public class ServerMessage extends MessageProtocol {
 	 * This constructor is required by Externalizable. There are no setters to
 	 * access the data, making a blank message. DO NOT USE!
 	 */
-	public ServerMessage() // Externalizable requires blank constructor
+	public ServerMessage()
 	{
 	}
 
 	/**
-	 * Constructs a message that contains a Command for Server to execute and a
-	 * list of arguments.
+	 * Constructs a message that contains the text from the user.
 	 * 
-	 * @param command
-	 *            Command to be executed by the Server
-	 * @param arguments
-	 *            arguments passed from the client for a given command
+	 * @param text
+	 *            The text that wass input by the user.
 	 */
 	public ServerMessage(String text)
 	{
