b3e9e843c5a129d4f0985494a68909b86d99fb1f
itrekkie <itrekkie@2e6ac136-260f-11de-98b4-f99e93c559b5>
2009-05-06 22:46:36 +0000

    add some basic tests for ArrayUtil

---
+++ trunk/test/util/ArrayUtilTest.java
@@ -0,0 +1,29 @@
+package util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ArrayUtilTest
+{
+	@Test
+	public void removeElement()
+	{
+		String[] nums = {"", "", "", ""};
+		assertEquals(4, nums.length);
+		
+		assertEquals(3, ArrayUtil.removeElement(nums, 0).length);
+	}
+
+	@Test
+	public void joinArguments()
+	{
+		List<String> words = Arrays.asList(new String[] {"hello", "world"});
+		assertEquals(2, words.size());
+		
+		assertEquals("hello,world,", ArrayUtil.joinArguments(words, ","));
+	}
+}
